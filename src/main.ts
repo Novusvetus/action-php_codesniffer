5364902986bde0890b59f9179767c33aeaeda224
Novusvetus / Marcel Rudolf <development@novusvetus.de>
2021-11-19 00:22:05 +0100

    All files option added and package updates

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import * as core from '@actions/core'
 import * as path from 'path'
-import {getChangedFiles} from './get-changed-file'
+import {getAllFiles} from './get-all-files'
+import {getChangedFiles} from './get-changed-files'
 import {runOnFiles} from './run-on-files'
 
 async function run(): Promise<void> {
   try {
-    const files = await getChangedFiles()
+    const scan_all = core.getBooleanInput('scan_all', {
+      required: false
+    })
+
+    let files
+    if (scan_all) {
+      files = await getAllFiles()
+    } else {
+      files = await getChangedFiles()
+    }
     core.info(JSON.stringify(files, null, 2))
     if (!files.files.length) {
       return
